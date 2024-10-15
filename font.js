15cfc3c4c9b815ba1d05a3daae54c1027d9eedcc
Orr Kislev <orrkislev@gmail.com>
2024-10-15 13:48:21 +0300

    .

---
+++ font.js
@@ -0,0 +1,454 @@
+let txt = "אז מה עושים עכשיו?";
+const border = 100
+const spaceSize = 5
+const lineHeight = 12
+const kerning = 1
+
+
+let lines = []
+
+function setup() {
+    initP5(false)
+    initPaper(true)
+    noLoop()
+
+    targetLines = max(round(4 * height / width), 1)
+    initGui()
+    rerun()
+}
+
+function rerun() {
+    createMolecules()
+    createLetters()
+    drawText()
+}
+
+function keyPressed() {
+    if (key == 'Enter') STROKE = !STROKE
+    if (key == 'Backspace') {
+        txt = txt.slice(0, -1)
+    }
+    if (key in letters || key == ' ') {
+        txt += key
+    }
+    drawText()
+}
+
+function drawText() {
+    paper.project.activeLayer.children.forEach(c => {
+        if (c.fillColor) c.remove()
+    })
+    chars = []
+    allMolecules = []
+    words = txt.split(' ').filter(w => w.length > 0).map(makeWord)
+    allText = new Group()
+    const wordsPerLine = ceil(words.length / targetLines)
+    let x = 0
+    let y = 0
+    words.forEach((word, i) => {
+        word.position = p(x, y)
+        allText.addChild(word)
+        x -= word.bounds.width + spaceSize
+        if ((i + 1) % wordsPerLine == 0) {
+            y += lineHeight
+            x = 0
+        }
+    })
+    makeDisplayed(allText)
+}
+
+
+function makeDisplayed(textGroup) {
+    let scaler = min((width - border * 2) / allText.bounds.width, (height - border * 2) / allText.bounds.height)
+    scaler = min(scaler, 100)
+    textGroup.scale(scaler, scaler)
+    textGroup.fillColor = 'black'
+    if (params.rounded > 0) {
+        textGroup.strokeColor = 'black'
+        textGroup.strokeWidth = .1 * scaler * params.rounded
+    }
+    if (params.stroke) {
+        textGroup.fillColor = 'white'
+        textGroup.strokeColor = 'black'
+        textGroup.strokeWidth = .1 * scaler
+    }
+    textGroup.position = p(width / 2, height / 2)
+}
+
+let words = []
+function makeWord(word) {
+    const wordGroup = new Group()
+    let x = 0
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i]
+        const char = makeChar(letter, x)
+        wordGroup.addChild(char)
+        x -= char.bounds.width + kerning
+    }
+    const pivotX = wordGroup.bounds.right
+    const pivotY = wordGroup.children.reduce((a, b) => {
+        if (a.pivot.y < b.pivot.y) return a
+        return b
+    }, wordGroup.children[0]).pivot.y
+    wordGroup.pivot = p(pivotX, pivotY)
+    wordGroup.children.forEach(editChar)
+    return wordGroup
+}
+
+let chars = []
+let allMolecules = []
+function makeChar(letter, x) {
+    const char = letters[letter].clone()
+    char.position = p(x - char.bounds.width, 0)
+    char.children.forEach(m => {
+        m.data.id = allMolecules.length
+        allMolecules.push(m)
+    })
+    char.data.id = chars.length
+    chars.push(char)
+    return char
+}
+
+function editChar(char) {
+    if (params.balagan > 0) {
+        const left = char.bounds.left
+        const right = char.bounds.right
+        const top = char.bounds.top
+        const bottom = char.bounds.bottom
+
+        const children = char.children
+        for (let j = 0; j < children.length * params.balagan; j++) {
+            const child = children[j]
+            // child.position = p(random(left, right), random(top, bottom))
+            child.position = p(
+                noise(child.data.id * 7) * (right - left) + left,
+                noise(child.data.id * 8 + 1) * (bottom - top) + top
+            )
+        }
+    }
+    char.children.forEach(m => {
+        let customRotation = rotationFolder.rotation
+        if (rotationFolder.randomize > noise(m.data.id)) {
+            customRotation *= noise(m.data.id * 5) * 2 - 1
+        }
+        if (rotationFolder.repeat > 1) {
+            for (let i = 1; i < rotationFolder.repeat; i++) {
+                const clone = m.clone()
+                clone.position = m.position
+                clone.rotate(i * customRotation)
+                char.addChild(clone)
+            }
+        } else m.rotate(customRotation)
+    })
+    char.children.forEach(m => {
+        const name = m.data.name
+        const scl = scaleFolder[name] * (1 + scaleFolder.randomize * (noise(m.data.id * 10) * 2 - 1))
+        m.scale(scl, scl)
+    })
+}
+
+
+// ---------------------------------------------
+// ----------------- GUI -----------------------
+// ---------------------------------------------
+
+const params = {
+    rounded: 0,
+    stroke: false,
+    balagan: 0
+}
+const scaleFolder = {
+    randomize: 0,
+    D: 1,
+    E: 1,
+    J: 1,
+    I: 1,
+    V: 1,
+    K: 1
+}
+const rotationFolder = {
+    randomize: 0,
+    rotation: 0,
+    repeat: 1
+}
+
+function initGui() {
+    const gui = new dat.GUI();
+
+    gui.add(params, 'balagan', 0, 1, 0.01).onChange(drawText)
+    gui.add(params, 'stroke').onChange(drawText)
+    gui.add(params, 'rounded', 0, 1, 0.01).onChange(rerun)
+
+    const scaleFolderGui = gui.addFolder('Scale')
+    scaleFolderGui.add(scaleFolder, 'randomize', 0, 1, 0.01).onChange(drawText)
+    scaleFolderGui.add(scaleFolder, 'D', 0.1, 2, 0.1).onChange(drawText)
+    scaleFolderGui.add(scaleFolder, 'E', 0.1, 2, 0.1).onChange(drawText)
+    scaleFolderGui.add(scaleFolder, 'J', 0.1, 2, 0.1).onChange(drawText)
+    scaleFolderGui.add(scaleFolder, 'I', 0.1, 2, 0.1).onChange(drawText)
+    scaleFolderGui.add(scaleFolder, 'V', 0.1, 2, 0.1).onChange(drawText)
+    scaleFolderGui.add(scaleFolder, 'K', 0.1, 2, 0.1).onChange(drawText)
+
+    const rotationFolderGui = gui.addFolder('Rotation')
+    rotationFolderGui.add(rotationFolder, 'rotation', -180, 180, 1).onChange(drawText)
+    rotationFolderGui.add(rotationFolder, 'repeat', 1, 10, 1).onChange(drawText)
+    rotationFolderGui.add(rotationFolder, 'randomize', 0, 1, 0.01).onChange(drawText)
+}
+
+
+// ---------------------------------------------
+// ----------------- Molecules -----------------
+// ---------------------------------------------
+
+let molecules = {}
+function createMolecules() {
+    const largeRadius = new paper.Size(params.rounded);
+    const smallRadius = new paper.Size(params.rounded * .5);
+
+    const DRect = new paper.Rectangle(p(0, 0), p(2, 2));
+    const D = new Path.Rectangle(DRect, largeRadius);
+    const ERect = new paper.Rectangle(p(0, 0), p(10, 2));
+    const E = new Path.Rectangle(ERect, largeRadius);
+    const JRect = new paper.Rectangle(p(0, 0), p(1, 2));
+    const J = new Path.Rectangle(JRect, smallRadius);
+    const IRect = new paper.Rectangle(p(0, 0), p(1, 4));
+    const I = new Path.Rectangle(IRect, smallRadius);
+    const V = new Path([p(0, 0), p(1, 0), p(5, 4), p(4, 4)])
+    V.closed = true
+    const K = new Path([p(0, 0), p(2, -2), p(3, -2), p(1, 0)])
+    K.closed = true
+
+    molecules = { D, E, J, I, V, K }
+    Object.keys(molecules).forEach(m => {
+        molecules[m].data = { name: m }
+    })
+}
+
+hebrewData = {
+    'א': [
+        ['D', 0, 0],
+        ['D', 2, 0],
+        ['J', 1, 2],
+        ['D', 1, 4],
+        ['V', 3, 2],
+        ['K', 6, 4],
+        ['D', 7, 0],
+    ],
+    'ב': [
+        ['E', 0, 0],
+        ['E', 0, 4],
+        ['J', 10, 4],
+        ['K', 7, 4],
+    ],
+    'ג': [
+        ['D', 0, 0],
+        ['K', 0, 6],
+        ['V', 1, 2],
+    ],
+    'ד': [
+        ['E', 0, 0],
+        ['K', 7, 4, true],
+        ['J', 9, 4,],
+    ],
+    'ה': [
+        ['E', 0, 0],
+        ['I', 9, 2],
+        ['J', 0, 4,],
+    ],
+    'ו': [
+        ['D', 0, 0],
+        ['I', 1, 2],
+    ],
+    'ז': [
+        ['D', 0, 0],
+        ['D', 2, 0],
+        ['K', 1, 4, true],
+        ['J', 3, 4],
+    ],
+    'ח': [
+        ['E', 0, 0],
+        ['I', 0, 2],
+        ['I', 9, 2],
+    ],
+    'ט': [
+        ['D', 4, 0],
+        ['D', 6, 0],
+        ['E', 0, 4],
+        ['I', 0, 0],
+        ['K', 7, 4, true],
+    ],
+    'י': [
+        ['D', 0, 0],
+        ['J', 1, 2],
+    ],
+    'כ': [
+        ['E', 0, 0],
+        ['E', 0, 4],
+        ['J', 9, 2],
+    ],
+    'ך': [
+        ['E', 0, 0],
+        ['I', 9, 2],
+        ['I', 9, 6],
+    ],
+    'ל': [
+        ['D', 0, 0],
+        ['D', 7, 8],
+        ['E', 0, 4],
+        ['J', 0, 2],
+        ['K', 7, 8],
+    ],
+    'מ': [
+        ['D', 5, 4],
+        ['D', 7, 4],
+        ['E', 1, 0],
+        ['J', 0, 0],
+        ['I', 1, 2],
+        ['K', 8, 4],
+    ],
+    'ם': [
+        ['E', 1, 0],
+        ['E', 1, 4],
+        ['J', 0, 0],
+        ['J', 1, 2],
+        ['J', 10, 2],
+    ],
+    'נ': [
+        ['D', 0, 0],
+        ['D', 0, 4],
+        ['D', 2, 4],
+        ['K', 1, 4, true],
+    ],
+    'ן': [
+        ['D', 0, 0],
+        ['I', 1, 2],
+        ['I', 1, 6],
+    ],
+    'ס': [
+        ['D', 0, 4],
+        ['D', 2, 4],
+        ['D', 4, 4],
+        ['D', 6, 4],
+        ['E', 0, 0],
+        ['J', 0, 2],
+        ['K', 7, 4],
+    ],
+    'ע': [
+        ['D', 0, 0],
+        ['D', 8, 0],
+        ['E', 0, 4],
+        ['J', 9, 2],
+        ['K', 1, 4, true],
+    ],
+    'פ': [
+        ['D', 0, 6],
+        ['D', 2, 6],
+        ['D', 4, 6],
+        ['E', 0, 0],
+        ['J', 0, 2],
+        ['V', 5, 2, true],
+    ],
+    'ף': [
+        ['D', 0, 0],
+        ['D', 2, 0],
+        ['D', 4, 0],
+        ['D', 6, 0],
+        ['I', 7, 2],
+        ['I', 7, 6],
+        ['K', 0, 4, true],
+    ],
+    'צ': [
+        ['D', 0, 0],
+        ['D', 2, 0],
+        ['D', 8, 0],
+        ['E', 0, 4],
+        ['K', 7, 4],
+        ['K', 3, 4, true],
+    ],
+    'ץ': [
+        ['D', 0, 0],
+        ['D', 2, 0],
+        ['D', 5, 0],
+        ['I', 5, 6],
+        ['K', 4, 4],
+        ['V', 1, 2],
+    ],
+    'ק': [
+        ['D', 7, 4],
+        ['D', 0, 8],
+        ['E', 0, 0],
+        ['I', 0, 4],
+        ['K', 7, 4],
+    ],
+    'ר': [
+        ['E', 0, 0],
+        ['I', 9, 2],
+    ],
+    'ש': [
+        ['D', 0, 0],
+        ['D', 2, 4],
+        ['D', 4, 0],
+        ['D', 4, 4],
+        ['D', 6, 4],
+        ['D', 8, 0],
+        ['D', 8, 4],
+        ['J', 5, 2],
+        ['J', 9, 2],
+        ['K', 0, 4, true],
+    ],
+    'ת': [
+        ['D', 0, 4],
+        ['D', 2, 4],
+        ['E', 1, 0],
+        ['I', 10, 2],
+        ['K', 1, 4, true],
+    ],
+}
+
+symbolData = {
+    '?': [
+        ['E', 0, 0],
+        ['K', 7, 4],
+        ['J', 7, 4],
+        ['D', 6.5, 8],
+    ],
+    '!': [
+        ['I', 0, 0],
+        ['J', 0, 4],
+        ['D', -.5, 8],
+    ],
+    '.': [
+        ['D', 0, 0],
+    ],
+    ':': [
+        ['D', 0, 0],
+        ['D', 0, 4],
+    ],
+    ',': [
+        ['K', -1, 6],
+    ],
+    ';': [
+        ['D', 0, 0],
+        ['K', -1, 6],
+    ],
+}
+
+let letters = {}
+function createLetters() {
+    letters = {}
+    const allData = { ...hebrewData, ...symbolData }
+    Object.keys(allData).forEach(letter => {
+        let letterGroup = new Group();
+        for (const [name, x, y, flipped] of allData[letter]) {
+            const molecule = molecules[name].clone();
+            molecule.translate(x, y);
+            if (flipped) molecule.scale(-1, 1);
+            molecule.pivot = molecule.bounds.topRight
+            letterGroup.addChild(molecule);
+            letterGroup.pivot = p(0, 0);
+        }
+        letters[letter] = letterGroup;
+    })
+
+    letters['ל'].pivot = p(0, 4)
+    letters['.'].pivot = p(0, -4)
+}
---
+++ utils.js
@@ -0,0 +1,25 @@
+const randomRange = (range) => random(range[0], range[1])
+const round_random = (a = 1, b = 0) => Math.floor(random(a, b + 1))
+const choose = (arr) => arr[Math.floor(random(arr.length))]
+const repeat = (n, func) => { for(let i = 0; i < n; i++) func(i) }
+
+Array.prototype.get = function get(i) {
+    return this[i % this.length]
+}
+const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const array2d = (w, h, fill = (i,j)=>{}) => Array.from({length: h}, (_, i) => Array.from({length: w}, (_, j) => fill(i,j)))
+const sum = (arr) => arr.reduce((a,b) => a + b, 0)
+
+Object.prototype.filter = function filter(func) {
+    const newObj = {}
+    for (let key in this) {
+        if (func(this[key], key)) newObj[key] = this[key]
+    }
+    return newObj
+}
+Object.prototype.length = function length() {
+    return Object.keys(this).length
+}
+Object.prototype.getKey = function getKey(i) {
+    return Object.keys(this)[i]
+}
